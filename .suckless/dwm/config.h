8c372be73e3a196b39df24e6999ce9a76b3f0769
BadBryan44567 <goddangamew05@gmail.com>
2023-07-07 19:34:25 +0530

    Change to Kitty Terminal

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           0,           0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,          -1,           -1 },
 	{ "st",       NULL,       NULL,       0,            0,           1,           0,           -1 },
+	{ "kitty",    NULL,       NULL,       0,            0,           1,           0,           -1 },
 	{ NULL,       NULL,      "Event",     0,            0,           0,           1,           -1 },   	
 };
 
@@ -71,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *roficmd[] = {"/usr/bin/rofi", "-show", "run"};
 static const char *filemanager[] = {"thunar", NULL};
 
